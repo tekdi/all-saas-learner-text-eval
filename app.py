8cf64eb90468bc340e3b54903cdd8f9cfb83e552
DevendraPPatil <devendra.patil@tekditechnologies.com>
2024-08-27 12:43:19 +0530

    Fix:Port chnages and console added

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ app.include_router(router)
 if __name__ == "__main__":
     import uvicorn
     num_workers = os.cpu_count() or 1
-    uvicorn.run("app:app", host="0.0.0.0", port=5003, debug=False, workers=num_workers)
+    uvicorn.run("app:app", host="0.0.0.0", port=5001, debug=False, workers=num_workers)
